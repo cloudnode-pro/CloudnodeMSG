ff27f3d0ba776b621e80403976217da94bb5eaab
Zefir Kirilov <contact@zefir.pro>
2024-01-06 18:49:27 +0200

    blank plugin

---
+++ src/main/java/pro/cloudnode/smp/cloudnodemsg/CloudnodeMSG.java
@@ -0,0 +1,17 @@
+package pro.cloudnode.smp.cloudnodemsg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CloudnodeMSG extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
