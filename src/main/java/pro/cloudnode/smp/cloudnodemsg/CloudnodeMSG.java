30b25b42b4d2775f376b05db89db1966d31c7a98
Zefir Kirilov <contact@zefir.pro>
2024-01-07 00:52:10 +0200

    plugin enable logic

--- src/main/java/pro/cloudnode/smp/cloudnodemsg/CloudnodeMSG.java
+++ src/main/java/pro/cloudnode/smp/cloudnodemsg/CloudnodeMSG.java
@@ -1,17 +1,34 @@
 package pro.cloudnode.smp.cloudnodemsg;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import pro.cloudnode.smp.cloudnodemsg.command.MessageCommand;
 
 public final class CloudnodeMSG extends JavaPlugin {
+    public static @NotNull CloudnodeMSG getInstance() {
+        return getPlugin(CloudnodeMSG.class);
+    }
+
+    public void reload() {
+        getInstance().reloadConfig();
+        getInstance().config.config = getInstance().getConfig();
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
+        reload();
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private final @NotNull PluginConfig config = new PluginConfig(getConfig());
+
+    public @NotNull PluginConfig config() {
+        return config;
     }
 }
