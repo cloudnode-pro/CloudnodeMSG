d5f68b42a3c9fdc97b2f95887a614fd74927b7dd
Nissoco23 <nissocomc@outlook.com>
2024-01-06 03:14:31 -0600

    Fixed dangerous bug that caused `/msg` to stop working at times

--- src/main/java/pro/cloudnode/smp/Commands/Message.java
+++ src/main/java/pro/cloudnode/smp/Commands/Message.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import static pro.cloudnode.smp.CloudnodeMSG.server;
 import static pro.cloudnode.smp.CloudnodeMSG.configFile;
+import static pro.cloudnode.smp.CloudnodeMSG.replyHashMap;
 
 public class Message implements @NotNull CommandExecutor {
     @Override
@@ -18,7 +19,6 @@ public class Message implements @NotNull CommandExecutor {
         if (!player.hasPermission("cloudnodemsg.use")) {
             player.sendMessage(MiniMessage.miniMessage().deserialize("<red>(!) You do not have permission to run that command!"));
             return true;
-
         }
 
         if (args.length == 0) {
@@ -37,18 +37,22 @@ public class Message implements @NotNull CommandExecutor {
         }
 
         for (@NotNull Player recipient: server.getOnlinePlayers()) {
+            System.out.println(recipient.getName());
             if (recipient.getName().equalsIgnoreCase(args[0])) {
                 args[0] = "";
-                StringBuilder stringBuilder = new StringBuilder();
+                @NotNull StringBuilder stringBuilder = new StringBuilder();
                 for (String arg: args) stringBuilder.append(arg).append(" ");
 
                 recipient.sendMessage(MiniMessage.miniMessage().deserialize(configFile.getString("incoming").replace("<sender>", player.getName()).replace("<message>", stringBuilder)));
-                player.sendMessage(MiniMessage.miniMessage().deserialize(configFile.getString("outgoing").replace("<recipient>", player.getName()).replace("<message>", stringBuilder)));
+                player.sendMessage(MiniMessage.miniMessage().deserialize(configFile.getString("outgoing").replace("<recipient>", recipient.getName()).replace("<message>", stringBuilder)));
+
+                replyHashMap.put(recipient.getUniqueId().toString(), player.getUniqueId().toString());
+                replyHashMap.put(player.getUniqueId().toString(), recipient.getUniqueId().toString());
+
                 return true;
             }
-            player.sendMessage(MiniMessage.miniMessage().deserialize("<red>(!) This user was not found. Check your spelling and try again!"));
-            return true;
         }
-        return false;
+        player.sendMessage(MiniMessage.miniMessage().deserialize("<red>(!) This user was not found or went offline. Check your spelling and try again!"));
+        return true;
     }
 }
