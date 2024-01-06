605f6034e256a9e9d0c633a6c45949ab7aaff6b4
Zefir Kirilov <contact@zefir.pro>
2024-01-07 00:50:10 +0200

    typed permissions

---
+++ src/main/java/pro/cloudnode/smp/cloudnodemsg/Permission.java
@@ -0,0 +1,10 @@
+package pro.cloudnode.smp.cloudnodemsg;
+
+import org.jetbrains.annotations.NotNull;
+
+public final class Permission {
+    /**
+     * Allows using the /msg and /r commands
+     */
+    public final static @NotNull String USE = "cloudnodemsg.use";
+}
