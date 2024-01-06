53a0faea96214bd133b58a28d3db68955d3dbebe
Nissoco23 <nissocomc@outlook.com>
2024-01-05 23:53:14 -0600

    Load configuration file in `CloudnodeMSG.java`

--- src/main/java/pro/cloudnode/smp/CloudnodeMSG.java
+++ src/main/java/pro/cloudnode/smp/CloudnodeMSG.java
@@ -1,11 +1,20 @@
 package pro.cloudnode.smp;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
 
 public final class CloudnodeMSG extends JavaPlugin {
+    public static @NotNull FileConfiguration configFile;
+    public static @NotNull HikariDataSource database;
 
     @Override
     public void onEnable() {
+        this.saveDefaultConfig();
+        configFile = this.getConfig();
         // Plugin startup logic
 
     }
