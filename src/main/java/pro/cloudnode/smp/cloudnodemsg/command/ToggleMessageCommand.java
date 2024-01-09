f1f886556788c6c8ac47bc34c1d5df10a24f50a9
Dviih <dviih2s@gmail.com>
2024-01-09 20:19:34 -0300

    Import and define required stuff for command executor.

--- src/main/java/pro/cloudnode/smp/cloudnodemsg/command/ToggleMessageCommand.java
+++ src/main/java/pro/cloudnode/smp/cloudnodemsg/command/ToggleMessageCommand.java
@@ -1,4 +1,16 @@
 package pro.cloudnode.smp.cloudnodemsg.command;
 
-public class ToggleMessageCommand {
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import pro.cloudnode.smp.cloudnodemsg.CloudnodeMSG;
+import pro.cloudnode.smp.cloudnodemsg.Permission;
+import pro.cloudnode.smp.cloudnodemsg.error.NoPermissionError;
+import pro.cloudnode.smp.cloudnodemsg.message.Message;
+
+import java.util.List;
+import java.util.Objects;
+
+public class ToggleMessageCommand extends Command {
 }
