0086a67ec3312ec0ea994fd72b02cbea4722bc65
Dviih <dviih2s@gmail.com>
2024-01-09 20:20:17 -0300

    The `onCommand` handler to deal with command sent.

--- src/main/java/pro/cloudnode/smp/cloudnodemsg/command/ToggleMessageCommand.java
+++ src/main/java/pro/cloudnode/smp/cloudnodemsg/command/ToggleMessageCommand.java
@@ -14,4 +14,26 @@ import java.util.Objects;
 
 public class ToggleMessageCommand extends Command {
     public static final @NotNull String usage = "<player>";
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!sender.hasPermission(Permission.TOGGLE) || (args.length == 1 && !sender.hasPermission(Permission.TOGGLE_OTHER))) return new NoPermissionError().send(sender);
+        if (args.length == 1) {
+            final @NotNull OfflinePlayer recipient = CloudnodeMSG.getInstance().getServer().getOfflinePlayer(args[0]);
+
+            Message.setNoIncoming(recipient);
+            sendMessage(sender, Message.getNoIncoming(recipient) ?
+                    CloudnodeMSG.getInstance().config().toggleDisableOther(Objects.requireNonNull(recipient.getName())) :
+                    CloudnodeMSG.getInstance().config().toggleEnableOther(Objects.requireNonNull(recipient.getName()))
+            );
+            return true;
+        }
+
+        Message.setNoIncoming(Objects.requireNonNull(Message.offlinePlayer(sender)));
+        sendMessage(sender, Message.getNoIncoming(Message.offlinePlayer(sender)) ?
+                CloudnodeMSG.getInstance().config().toggleDisable() :
+                CloudnodeMSG.getInstance().config().toggleEnable()
+        );
+        return true;
+    }
+
 }
